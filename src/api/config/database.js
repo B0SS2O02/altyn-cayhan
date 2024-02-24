a0cccc2c324e44007cfcca5839cf5ccaa6153483
serdar8055 <serdarsaryjayev2002@gmail.com>
2024-02-24 18:50:56 +0500

    update

--- src/api/config/database.js
+++ src/api/config/database.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 const config = require("../../../config").development;
 
+console.log(config.database);
+
 let sequelize = new Sequelize(config.database);
 
 module.exports = sequelize;
