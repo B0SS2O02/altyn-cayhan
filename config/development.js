4c40c349b9e1c77fbb487cfeb2337f812ba75f55
shahzod1609 <hallievshahzad7@gmail.com>
2024-03-05 07:53:17 +0000

    db config edit

--- config/development.js
+++ config/development.js
@@ -1,34 +1,9 @@
-module.exports = {
-<<<<<<< HEAD
-    database: {
-      dialect: "postgres",
-      username: "root",
-      password: "root14122023",
-      database: "altyncayhana",
-      host: "127.0.0.1",
-      port: 5432,
-      logging: false,
-      dialectOptions: {
-        maxRetryCount: 10, // Maximum number of connection acquisition retries
-        minRetryCount: 1, // Minimum number of connection acquisition retries
-        retryInterval: 1000, // Time interval between retries in milliseconds (1 second in this example)
-      },
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-=======
+module.exports={
   database: {
     dialect: "postgres",
-    // username: "root",
-    // password: "root5312023",
-    // host: "95.85.127.110",
-    username: "postgres",
-    password: "pikir",
+    username: "root",
+    password: "root14122023",
     host: "127.0.0.1",
-
     database: "altyncayhana",
     port: 5432,
     logging: false,
@@ -36,7 +11,6 @@ module.exports = {
       maxRetryCount: 10, // Maximum number of connection acquisition retries
       minRetryCount: 1, // Minimum number of connection acquisition retries
       retryInterval: 1000, // Time interval between retries in milliseconds (1 second in this example)
->>>>>>> 712ec142eba3fe9ccb1b7f5213e1124b66a5da1e
     },
     pool: {
       max: 5,
@@ -44,14 +18,8 @@ module.exports = {
       acquire: 30000,
       idle: 10000,
     },
-<<<<<<< HEAD
-  };
-  
-  
-=======
   },
   key: {
     JWT_SECRET_KEY: "futboljankoyer__!",
   },
 };
->>>>>>> 712ec142eba3fe9ccb1b7f5213e1124b66a5da1e
