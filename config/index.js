ab3400a218d2b61ca9c6a6f5208f8985ac9c890f
serdar8055 <serdarsaryjayev2002@gmail.com>
2024-03-07 20:53:54 +0500

    gitignore update

--- config/index.js
+++
@@ -1,9 +0,0 @@
-const development = require('./development')
-const production = require('./production')
-const test = require('./test')
-
-module.exports = {
-    development,
-    production,
-    test
-}
