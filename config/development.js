1073485eef32720003b2e8cba358b9d291f52553
serdar8055 <serdarsaryjayev2002@gmail.com>
2024-03-07 20:59:55 +0500

    update

---
+++ config/development.js
@@ -0,0 +1,25 @@
+module.exports = {
+  database: {
+    database: "altyncayhana",
+    username: "root",
+    password: "root14122023",
+    dialect: "postgres",
+    host: "127.0.0.1",
+    port: 5432,
+    logging: true,
+    dialectOptions: {
+      maxRetryCount: 10,
+      minRetryCount: 1,
+      retryInterval: 1000,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+  key: {
+    JWT_SECRET_KEY: "futboljankoyer__!",
+  },
+};
